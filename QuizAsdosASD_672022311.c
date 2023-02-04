3a8519fa8203b753593d39c19b312674fdee8341
syahdanstr <123710756+syahdanstr@users.noreply.github.com>
2023-02-04 20:32:35 +0700

    Update QuizAsdosASD_672022311.c

--- QuizAsdosASD_672022311.c
+++ QuizAsdosASD_672022311.c
@@ -2,12 +2,6 @@
 #include <conio.h>
 #include <windows.h>
 
-void setColor(unsigned short color)
-{
-    HANDLE hCon = GetStdHandle(STD_OUTPUT_HANDLE);
-
-    SetConsoleTextAttribute(hCon, color);
-}
 void gotoxy(int x, int y)
 {
     COORD coord;
